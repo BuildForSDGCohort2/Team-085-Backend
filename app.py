f39912d0d36e79726f21478f9dba8f3c4a9c3df4
isaac-adedoyin <aderonmuisaac@gmail.com>
2020-08-29 14:32:26 +0100

    feat: flask app configuration
    Bound Flask application to SQLAlchemy service. Imported Users and Jobs tables.

--- app.py
+++ app.py
@@ -1,14 +1,21 @@
-import os
+# import os
 from flask import Flask
 from models.models import setup_db
 
-'''
+"""
 App Config
     Creates the flask app
-'''
-
-
+"""
 app = Flask(__name__)
+db = setup_db(app)
+
+"""
+Models
+    Imports models from models/models.py file
+    The reason the models are not being imported at the top is due to \
+    errors that may arise from the models being created before the flask app
+"""
+from models.models import Users, Jobs
 
 if __name__ == '__main__':
     app.run(port=8080, debug=True)
